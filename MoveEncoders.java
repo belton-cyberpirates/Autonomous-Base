4b2dabb886e80862a7da1773659c3cc7a527d91d
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-11-14 07:42:35 -0600

    fix MoveEncoders

--- MoveEncoders.java
+++ MoveEncoders.java
@@ -5,25 +5,34 @@ import org.firstinspires.ftc.teamcode.Direction;
 
 
 public class Move extends Action {
-	Auto auto;
-	Direction dir;
-	double distance;
-	ElapsedTime startTime;
-	
-	public Move(Auto auto, Direction dir, double distance) {
-		this.auto = auto;
-		
-		this.dir = dir;
-		this.distance = distance;
-	}
+    Auto auto;
+    Direction dir;
+    int distance;
+    ElapsedTime startTime;
+    double expectedTime;
+    
+    public Move(Auto auto, Direction dir, int distance) {
+        this.auto = auto;
+        
+        this.dir = dir;
+        this.distance = distance;
+    }
+    
+    public Move(Auto auto, Direction dir, int distance, double expectedTime) {
+        this.auto = auto;
+        
+        this.dir = dir;
+        this.distance = distance;
+        this.expectedTime = expectedTime;
+    }
 
-	public void onStart() {
-		startTime = new ElapsedTime();
-		auto.driveMotors.Move(dir, distance);
-	}
-	
-	public boolean isDone() {
-		return auto.driveMotors.isDone() || (expectedTime != 0 && startTime.time() >= expectedTime);
-	}
+    public void onStart() {
+        startTime = new ElapsedTime();
+        auto.driveMotors.MoveWithEncoders(dir, distance);
+    }
+    
+    public boolean isDone() {
+        return auto.driveMotors.isDone() /*|| (expectedTime != 0 && startTime.time() >= expectedTime*/;
+    }
 
 }
